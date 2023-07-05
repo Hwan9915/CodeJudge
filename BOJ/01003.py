df26c157593a606b8c7259f1bfa7646079b0f28d
hwan9915 <cubic0915@naver.com>
2023-07-05 16:19:21 +0900

    2023-07-05 silver 3

---
+++ BOJ/01003.py
@@ -0,0 +1,20 @@
+# silver 3
+# 1003번, 피보나치 함수
+
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+
+dp = [[0, 0] for _ in range(41)]
+dp[0][0] = 1
+dp[1][1] = 1
+
+for i in range(2,41):
+    dp[i][0] = dp[i-1][0] + dp[i-2][0]
+    dp[i][1] = dp[i-1][1] + dp[i-2][1]
+
+for i in range(N):
+    temp = int(input())
+    print(dp[temp][0], dp[temp][1])
