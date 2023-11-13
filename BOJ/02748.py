7cb05f4d1ffcfe5ccbbb768daca489657547758c
hwan9915 <cubic0915@naver.com>
2023-11-13 13:09:07 +0900

    2023-11-13 bronze

--- BOJ/02748.py
+++ BOJ/02748.py
@@ -3,9 +3,14 @@
 import sys
 input = sys.stdin.readline
 
+# 피보나치 배열 초기값
 fibo = [0,1,1]
+
+# n 값 입력
 n = int(input())
 
+
+# n까지 피보나치 배열 메모이제이션
 for i in range(2,n):
     fibo.append(fibo[-1]+fibo[-2])
 
