21305b29e2eda1f7a218cde0e02e7e22f310cd5c
hwan9915 <cubic0915@naver.com>
2023-11-14 19:15:02 +0900

    2023-11-13 bronze

---
+++ BOJ/12865.py
@@ -0,0 +1,14 @@
+import sys
+
+input = sys.stdin.readline
+
+N, K = map(int, input().split())
+
+cache = [0] * (K + 1)
+
+for _ in range(N):
+    w, v = map(int, input().split())
+    for i in range(K, w - 1, -1):
+        cache[i] = max(cache[i], v + cache[i - w])
+
+print(cache[-1])
