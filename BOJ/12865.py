680a439dcb114b80b68840bc3f55b66646975b7d
hwan9915 <cubic0915@naver.com>
2023-11-15 19:27:10 +0900

    2023-11-15 bronze

--- BOJ/12865.py
+++ BOJ/12865.py
@@ -1,13 +1,17 @@
 import sys
-
 input = sys.stdin.readline
 
+# N과 K 입력
 N, K = map(int, input().split())
 
+# 저장할 배열 초기화
 cache = [0] * (K + 1)
 
 for _ in range(N):
+    # 과자 무게, 과자 가치
     w, v = map(int, input().split())
+
+    # 가방 최대로 넣는 계산
     for i in range(K, w - 1, -1):
         cache[i] = max(cache[i], v + cache[i - w])
 
