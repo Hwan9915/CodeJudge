a4f16094e192ee16d81024054fa71cf8b20b346b
hwan9915 <cubic0915@naver.com>
2023-11-22 09:05:09 +0900

    2023-11-21 gold

--- BOJ/01038.py
+++ BOJ/01038.py
@@ -10,7 +10,6 @@ for i in range(1,11):
     for j in combinations(comb,i):
         ans.append(int(''.join(list(map(str,reversed((j)))))))
 
-# 10 C 10  0,1,2,3,4,5,6,7,8,9 9876543210
 ans.sort()
 
 n = int(input())
