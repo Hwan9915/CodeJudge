891674ad098cbcd33140bd387a5365a8cdf79449
hwan9915 <cubic0915@naver.com>
2023-12-18 02:07:21 +0900

    2023-12-18 silver

--- BOJ/20291.py
+++ BOJ/20291.py
@@ -13,7 +13,7 @@ def main():
             file[file_extension] += 1
     file_extension_lst = sorted(file.keys())
     for i in file_extension_lst:
-        print(f'{i} {file[i]}\n')
+        print(f'{i} {file[i]}')
 
 if __name__ == '__main__':
     input = sys.stdin.readline
