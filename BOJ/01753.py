27f5182385eb440ff5bff694a9d32c8207dec42c
hwan9915 <cubic0915@naver.com>
2023-12-18 02:07:00 +0900

    2023-12-18 gold

--- BOJ/01753.py
+++ BOJ/01753.py
@@ -38,9 +38,9 @@ def main():
 
     for i in range(1, vertex + 1):
         if dp[i] == INF:
-            print('INF\n')
+            print('INF')
         else:
-            print('%d\n'%dp[i])
+            print('%d'%dp[i])
 
 
 if __name__ == "__main__":
