0e229d172988bd7e6883d02337f61a507ed90967
hwan9915 <cubic0915@naver.com>
2024-07-21 17:48:03 +0900

    24-07-11 n^2 배열 자르기

--- programmers/LEVEL 2/87390.py
+++ programmers/LEVEL 2/87390.py
@@ -13,5 +13,12 @@ def solution(n, left, right):
     return answer
 
 
+def solution1(n, left, right):
+    answer = []
+    for i in range(left,right+1):
+        answer.append(max(i//n,i%n)+1)
+    return answer
+
+
 if __name__ == "__main__":
-    print(solution(4,7,14))
+    print(solution1(4,0,14))
