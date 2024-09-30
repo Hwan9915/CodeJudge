ba10c42f4d3ea38d7e78fbb2805216278aad915a
hwan9915 <cubic0915@naver.com>
2024-09-30 11:16:01 +0900

    24-09-30 silver

---
+++ BOJ/32372.py
@@ -0,0 +1,48 @@
+def main():
+    n, m = map(int, input().split())
+    lst = []
+
+    for a in range(1, n+1):
+        for b in range(1, n+1):
+            lst.append((a,b))
+    na_lst = [tuple(map(int, input().split())) for _ in range(m)]
+
+    for x,y,d in na_lst:
+        new_lst = []
+        if d == 1:
+            for a in lst:
+                if a[0] < x and a[1] == y:
+                    new_lst.append(a)
+        if d == 2:
+            for a in lst:
+                if a[0] < x and a[1] > y:
+                    new_lst.append(a)
+        if d == 3:
+            for a in lst:
+                if a[0] == x and a[1] > y:
+                    new_lst.append(a)
+        if d == 4:
+            for a in lst:
+                if a[0] > x and a[1] > y:
+                    new_lst.append(a)
+        if d == 5:
+            for a in lst:
+                if a[0] > x and a[1] == y:
+                    new_lst.append(a)
+        if d == 6:
+            for a in lst:
+                if a[0] > x and a[1] < y:
+                    new_lst.append(a)
+        if d == 7:
+            for a in lst:
+                if a[0] == x and a[1] < y:
+                    new_lst.append(a)
+        if d == 8:
+            for a in lst:
+                if a[0] < x and a[1] < y:
+                    new_lst.append(a)
+        lst = new_lst
+    print(lst[0][0], lst[0][1])
+
+if __name__ == "__main__":
+    main()
---
+++ BOJ/32373.py
@@ -0,0 +1,23 @@
+def main():
+    n, k = map(int, input().split())
+
+    lst = list(map(int, input().split()))
+
+    if lst == [i for i in range(n)]:
+        return 'Yes'
+
+    set_ = set()
+    for idx, i in enumerate(lst):
+        if idx != i:
+            set_.add(abs(idx-i))
+
+    list_ = list(set_)
+    for i in list_:
+        if i % k != 0:
+            return "No"
+    else:
+        return "Yes"
+
+
+if __name__ == "__main__":
+    print(main())
