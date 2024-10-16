31973b65a5e083ef9c268b150f7965245f68a73f
hwan9915 <cubic0915@naver.com>
2024-10-16 20:44:13 +0900

    2024-10-16 silver

---
+++ BOJ/01063.py
@@ -0,0 +1,63 @@
+import sys
+
+input = sys.stdin.readline
+
+def main():
+    pos_dict = {
+        'A' : 1,
+        'B' : 2,
+        'C' : 3,
+        'D' : 4,
+        'E' : 5,
+        'F' : 6,
+        'G' : 7,
+        'H' : 8
+    }
+    move_dict ={
+        'R' : [1, 0],
+        'L' : [-1, 0],
+        'B' : [0, -1],
+        'T' : [0, 1],
+        'RT' : [1, 1],
+        'LT' : [-1, 1],
+        'RB' : [1, -1],
+        'LB' : [-1, -1]
+    }
+    k, s, n = list(input().rstrip().split())
+    
+    k = [pos_dict[k[0]], int(k[1])]
+    s = [pos_dict[s[0]], int(s[1])]
+    n = int(n)
+    
+    for i in range(n):
+        move = move_dict[input().rstrip()]
+        # 킹이 돌 위치로 가는가
+        if k[0] + move[0] == s[0] and k[1] + move[1] == s[1]:
+            # 돌이 안 나가는가
+            if 0 < s[0] + move[0] and s[0] +move[0] < 9 and 0 < s[1] + move[1] and s[1] + move[1] < 9:
+                s[0] += move[0]
+                s[1] += move[1]
+                k[0] += move[0]
+                k[1] += move[1]
+            else:
+                continue
+        else:
+            if 0 < k[0] + move[0] and k[0] +move[0] < 9 and 0 < k[1] + move[1] and k[1] + move[1] < 9:
+                k[0] += move[0]
+                k[1] += move[1]
+    
+    
+    print(chr(k[0]+64) + str(k[1]))
+    print(chr(s[0]+64) + str(s[1]))
+        
+    # 킹 -> 돌
+     # 킹이 돌 위치로 갈 때, 돌이 나가지 않는다면 둘다 이동
+     # 킹이 돌 위치로 갈 때, 돌이 나간다면 둘 다 이동하지 않음
+    #킹이 돌 위치로 가지 않을 때, 킹만 이동
+    # 킹
+    
+        
+    
+    
+if __name__ == "__main__":
+    main()
